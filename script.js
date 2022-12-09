3418fff82e29cea979243a4265e4c90e572b4f23
Samir Mandal <113394127+Samirmandal84@users.noreply.github.com>
2022-12-09 05:36:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+alert(`❤️Good Morning Baby❤️`);
+alert(`Acha ek Baat Bolu🥺👉🏻👈🏻`);
+alert(`Baacha, I'm Really SORRY for yesterday😔`);
+alert(`🤭You Paglu Baccha🫣`);
+alert(`MuuuAAAAAhhhhhhhhhhhhh❤️`)
